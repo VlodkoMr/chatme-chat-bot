c7867823f398db741189ecc5043095f846e84330
Vlodkow <vlodkow@gmail.com>
2023-01-13 19:50:57 +0200

    Testnet limit

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -96,7 +96,7 @@ const getOpenAIResponse = async (requestText: string) => {
     model: "text-davinci-003",
     prompt: requestText,
     temperature: 0, // Higher values means the model will take more risks.
-    max_tokens: 2048, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+    max_tokens: 4096, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
     top_p: 1, // alternative to sampling with temperature, called nucleus sampling
     frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
     presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
@@ -158,6 +158,15 @@ export const processMessage = async (message: ProcessMessage) => {
       `(like)`
     );
   }
+
+  // Limit AI requests from testnet
+  // if (NEAR_NETWORK !== 'mainnet') {
+  //   return sendReplyMessage(
+  //     message.toAddress,
+  //     message,
+  //     `Sorry, AI available only on mainnet`
+  //   );
+  // }
 
   const response = await getOpenAIResponse(decodedText);
   const responseText = response.data.choices[0].text.trim();
